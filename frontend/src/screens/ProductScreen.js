62866e19427ac14f2c0988ae12e5d8d812c7542b
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2020-12-05 20:56:09 +0100

    product page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,70 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
 
-const ProductScreen = () => {
-  return <div>Product</div>;
+const ProductScreen = ({ match }) => {
+  //Da se uskladi ID kod Routera kada idemo na određeni proizvod
+  const product = products.find((product) => product._id === match.params.id);
+
+  return (
+    <div>
+      <Link className="btn btn-dark my-3" to="/">
+        Nazad
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={` ${product.numReviews} recenzija`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Cijena: {product.price} KM</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Cijena:</Col>
+                  <Col>
+                    <strong>{product.price} KM</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Zaliha:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "Na stanju" : "Nema na stanju"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Dodaj u korpu
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default ProductScreen;
