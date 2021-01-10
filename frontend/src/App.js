0a3feabb13ff3b4279ce866a48d7ee781ee384bf
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-01-10 17:32:43 +0100

    user login screen & func

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/login" component={LoginScreen} />
         </Container>
       </main>
       <Footer />
