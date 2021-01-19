ef6fb61e4a0997e581972925d87ba5e2256aaefa
unknown <asko2212@hotmail.com>
2021-01-19 14:47:34 +0100

    admin user update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -124,4 +124,41 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser };
+// @desc Get user by ID
+// @route GET /api/users/:id
+// @access Private/Admin 
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if(user){
+    res.json(user)
+  }else{
+    res.status(404)
+    throw new Error('Korisnik nije pronađen.')
+  }
+});
+
+// @desc Update user
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin
+
+    const updateUser = user.save();
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Korisnik nije pronađen");
+  }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser };
